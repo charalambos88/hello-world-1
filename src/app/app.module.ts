64e10b3bd03b5adf6fc4b0a3983d8c99e26f5040
charalambos88 <charalambos88@users.noreply.github.com>
2017-05-22 11:49:44 +0300

    deployed mode
    change deployed mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
 
 import { AppComponent } from './app.component';
 
